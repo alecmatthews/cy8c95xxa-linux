db1de55b16c79b32dafdb7a2dcc0f624d759cd4c
Alec Matthews <me@alecmatthews.dev>
2022-07-06 16:17:56 -0700

    Created simple char driver to toggle one I/O bank
    Everything is working as expected. The charecter driver si correctly
    using the I2C bus to write data from the user. The next step would be to
    explore the libgpio framework.

--- gpio-cy8c95xxa.c
+++ gpio-cy8c95xxa.c
@@ -1,5 +1,6 @@
 #include <linux/i2c.h>
 #include <linux/module.h>
+#include <linux/miscdevice.h>
 
 enum cypress_ioexpander_type {
     CYP_TYPE_20A,
@@ -7,14 +8,110 @@ enum cypress_ioexpander_type {
     CYP_TYPE_60A
 };
 
+struct ioexp_dev {
+    struct i2c_client *c;
+    struct miscdevice ioexp_miscdev;
+    char *name;
+};
+
+#define REG_OUTPUT_PORT0 0x08
+
+static ssize_t ioexp_read_file (struct file *file, char __user *buf,
+                                size_t count, loff_t *ppos)
+{
+    struct ioexp_dev *ioexp;
+    s32 recv_data;
+    int size;
+    char to_user[32];
+
+    ioexp = container_of(file->private_data, struct ioexp_dev, ioexp_miscdev);
+    dev_info(&ioexp->c->dev, "Reading from device...");
+
+    recv_data = i2c_smbus_read_byte_data(ioexp->c, REG_OUTPUT_PORT0);
+    if (recv_data < 0)
+        return -EIO;
+
+    size = snprintf(to_user, ARRAY_SIZE(to_user), "0x%02X\n", recv_data);
+
+    if (*ppos != 0)
+        return 0;
+
+    if(copy_to_user(buf, to_user, size))
+        return -EFAULT;
+
+    *ppos += size;
+    return size;
+}
+
+static ssize_t ioexp_write_file (struct file *file, const char __user *buf,
+                                 size_t count, loff_t *ppos)
+{
+    struct ioexp_dev *ioexp;
+    int ret;
+    u8 reg_value;
+
+    ioexp = container_of(file->private_data, struct ioexp_dev, ioexp_miscdev);
+    dev_info(&ioexp->c->dev, "Writing to device...");
+
+    if (*ppos != 0)
+        return -EFAULT;
+
+    ret = kstrtou8_from_user(buf, count, 0, &reg_value);
+    if (ret != 0)
+        return -EINVAL;
+
+    dev_info(&ioexp->c->dev, "Value to write: 0x%02X", reg_value);
+
+    ret = i2c_smbus_write_byte_data(ioexp->c, REG_OUTPUT_PORT0, reg_value);
+    if (ret < 0)
+        return -EIO;
+
+    return count;
+}
+
+static const struct file_operations ioexp_fops = {
+    .owner = THIS_MODULE,
+    .read = ioexp_read_file,
+    .write = ioexp_write_file,
+};
+
 static int cy8c95xxa_probe(struct i2c_client *client)
 {
+    static u8 dev_count = 0;
+    struct ioexp_dev *ioexp;
+
     dev_info(&client->dev, "Device detected");
+
+    ioexp = devm_kzalloc(&client->dev, sizeof(*ioexp), GFP_KERNEL);
+    if (!ioexp)
+        return -ENOMEM;
+
+    ioexp->c = client;
+    ioexp->name = devm_kasprintf(&client->dev, GFP_KERNEL, "ioexp%02d", dev_count++);
+    ioexp->ioexp_miscdev.name = ioexp->name;
+    ioexp->ioexp_miscdev.minor = MISC_DYNAMIC_MINOR;
+    ioexp->ioexp_miscdev.fops = &ioexp_fops;
+
+    i2c_set_clientdata(client, ioexp);
+    dev_info(&client->dev, "Device allocated");
+
+    if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE_DATA)) {
+        dev_err(&client->dev, "I2C bus incompatible");
+        return -EIO;
+    }
+
+    misc_register(&ioexp->ioexp_miscdev);
+
+    dev_info(&client->dev, "Probe finished");
     return 0;
 }
 
 static int cy8c95xxa_remove(struct i2c_client *client)
 {
+    struct ioexp_dev *ioexp;
+    ioexp = i2c_get_clientdata(client);
+    misc_deregister(&ioexp->ioexp_miscdev);
+
     dev_info(&client->dev, "Device removed");
     return 0;
 }
