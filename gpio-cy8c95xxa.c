bfa58df4d4dbe70a29456c8d8d40df4f63c11d21
Alec Matthews <me@alecmatthews.dev>
2022-08-08 14:11:43 -0700

    Finished first version of driver

--- gpio-cy8c95xxa.c
+++ gpio-cy8c95xxa.c
@@ -1,18 +1,18 @@
 #include <linux/i2c.h>
-#include <linux/module.h>
-#include <linux/miscdevice.h>
-#include <linux/of_device.h>
-
-#include <linux/irq.h>
-
-#include <linux/mutex.h>
-
 #include <linux/gpio/driver.h>
+#include <linux/irq.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/of_device.h>
 
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/pinctrl/pinconf-generic.h>
 #include <linux/pinctrl/pinconf.h>
-#include <linux/pinctrl/consumer.h>
+#include <linux/pinctrl/pinmux.h>
+
+#include <linux/pwm.h>
+
+#include <asm/div64.h>
 
 #define MODULE_NAME "gpio-cy8c95xxa"
 
@@ -33,15 +33,31 @@
  * To modify the port first select it, then change the behavior with
  * the setting register.
  */
-#define REG_PORT_SELECT 0x18
-#define REG_INT_MASK    0x19
-#define REG_PIN_DIR     0x1C
+#define REG_PORT_SELECT 				0x18
+#define REG_INT_MASK    				0x19
+#define REG_PWM_EAN						0x1A
+#define REG_INVERSION					0x1B
+#define REG_PIN_DIR     				0x1C
+#define REG_DRV_MODE_PULL_UP			0x1D
+#define REG_DRV_MODE_PULL_DWN			0x1E
+#define REG_DRV_MODE_OPEN_DRAIN_HIGH	0x1F
+#define REG_DRV_MODE_OPEN_DRAIN_LOW		0x20
+#define REG_DRV_MODE_STRONG				0x21
+#define REG_DRV_MODE_HIGH_Z				0x23
+
+#define REG_PWM_SELECT	0x28
+#define REG_PWM_PERIOD	0x2A
+#define REG_PWM_PULSE_W	0x2B
 
 /* Chip properties */
 // TODO: this will be different for the other chip models!
-#define NGPIO 20
+#define NGPIO	20
 /* Number of ports in the larges chip. */
-#define NPORTS 8
+#define NPORTS	8
+/* Number of PWM channels; TODO: up to 16 for larger chips... */
+#define NPWM		4
+#define PWM_BASE	0
+#define PWM_CLK_NS	31250
 
 #define SET_BIT(nr, p)		(*p |= BIT(nr))
 #define CLEAR_BIT(nr, p)	(*p &= ~BIT(nr))
@@ -71,6 +87,8 @@ struct cy8c95xxa {
 
 	struct gpio_chip chip;
 
+	struct pwm_chip pwm_chip;
+
 	struct pinctrl_desc pinctrl;
 	struct pinctrl_dev *pctldev;
 	struct pinctrl *pin_ctrl;
@@ -82,80 +100,6 @@ struct cy8c95xxa {
 	struct device *dev;
 
 	char *name;
-};
-
-#define CY8C95XXA_PIN(a)	PINCTRL_PIN(a, "gpio" #a)
-struct pinctrl_pin_desc pinctrl_pins_20a[] = {
-	CY8C95XXA_PIN(0),
-	CY8C95XXA_PIN(1),
-	CY8C95XXA_PIN(2),
-	CY8C95XXA_PIN(3),
-	CY8C95XXA_PIN(4),
-	CY8C95XXA_PIN(5),
-	CY8C95XXA_PIN(6),
-	CY8C95XXA_PIN(7),
-	CY8C95XXA_PIN(8),
-	CY8C95XXA_PIN(9),
-	CY8C95XXA_PIN(10),
-	CY8C95XXA_PIN(11),
-	CY8C95XXA_PIN(12),
-	CY8C95XXA_PIN(13),
-	CY8C95XXA_PIN(14),
-	CY8C95XXA_PIN(15),
-	CY8C95XXA_PIN(16),
-	CY8C95XXA_PIN(17),
-	CY8C95XXA_PIN(18),
-	CY8C95XXA_PIN(19),
-};
-
-static int cy8c95xxa_pin_config_get(struct pinctrl_dev *pctldev,
-				    unsigned pin, unsigned long *config)
-{
-	return -ENOTSUPP;
-}
-
-static int cy8c95xxa_pin_config_set(struct pinctrl_dev *pctldev,
-				    unsigned pin,
-				    unsigned long *configs,
-				    unsigned num_configs)
-{
-	struct cy8c95xxa *cyp = pinctrl_dev_get_drvdata(pctldev);
-	enum pin_config_param param;
-	int i;
-
-	dev_info(cyp->dev, "For pin %d the parameters are:", pin);
-
-	// print all configs...
-	for (i = 0; i < num_configs; i++) {
-		param = pinconf_to_config_param(configs[i]);
-		dev_info(cyp->dev, "\tparameter: %d", param);
-	}
-
-	return 0;
-}
-
-static const struct pinconf_ops cy8_pinconf_ops = {
-	.pin_config_get = cy8c95xxa_pin_config_get,
-	.pin_config_set = cy8c95xxa_pin_config_set,
-	.is_generic = true,
-};
-
-static int cy8c95xxa_get_groups_count(struct pinctrl_dev *pctrldev)
-{
-	return 0;
-}
-
-static const char *cy8c95xxa_get_group_name(struct pinctrl_dev *pctldev,
-					    unsigned selector)
-{
-	return NULL;
-}
-
-static const struct pinctrl_ops cy8_pinctrl_ops = {
-	.get_groups_count = cy8c95xxa_get_groups_count,
-	.get_group_name = cy8c95xxa_get_group_name,
-	.dt_node_to_map = pinconf_generic_dt_node_to_map_all,
-	.dt_free_map = pinconf_generic_dt_free_map,
 };
 
 // TODO: add offsets for larger chips
@@ -189,6 +133,255 @@ static int cyp_write(struct cy8c95xxa *cyp, u8 reg, u8 val)
 {
 	return i2c_smbus_write_byte_data(cyp->i2c_client, reg, val);
 }
+
+#define CY8C95XXA_PIN(a)	PINCTRL_PIN(a, "gpio" #a)
+struct pinctrl_pin_desc pinctrl_pins_20a[] = {
+	CY8C95XXA_PIN(0),
+	CY8C95XXA_PIN(1),
+	CY8C95XXA_PIN(2),
+	CY8C95XXA_PIN(3),
+	CY8C95XXA_PIN(4),
+	CY8C95XXA_PIN(5),
+	CY8C95XXA_PIN(6),
+	CY8C95XXA_PIN(7),
+	CY8C95XXA_PIN(8),
+	CY8C95XXA_PIN(9),
+	CY8C95XXA_PIN(10),
+	CY8C95XXA_PIN(11),
+	CY8C95XXA_PIN(12),
+	CY8C95XXA_PIN(13),
+	CY8C95XXA_PIN(14),
+	CY8C95XXA_PIN(15),
+	CY8C95XXA_PIN(16),
+	CY8C95XXA_PIN(17),
+	CY8C95XXA_PIN(18),
+	CY8C95XXA_PIN(19),
+};
+
+#define CY8C95XXA_GROUP(a)	"gpio" #a
+static const char *const cy8c95xxa_group_names[] = {
+	CY8C95XXA_GROUP(0),
+	CY8C95XXA_GROUP(1),
+	CY8C95XXA_GROUP(2),
+	CY8C95XXA_GROUP(3),
+	CY8C95XXA_GROUP(4),
+	CY8C95XXA_GROUP(5),
+	CY8C95XXA_GROUP(6),
+	CY8C95XXA_GROUP(7),
+	CY8C95XXA_GROUP(8),
+	CY8C95XXA_GROUP(9),
+	CY8C95XXA_GROUP(10),
+	CY8C95XXA_GROUP(11),
+	CY8C95XXA_GROUP(12),
+	CY8C95XXA_GROUP(13),
+	CY8C95XXA_GROUP(14),
+	CY8C95XXA_GROUP(15),
+	CY8C95XXA_GROUP(16),
+	CY8C95XXA_GROUP(17),
+	CY8C95XXA_GROUP(18),
+	CY8C95XXA_GROUP(19),
+};
+
+#define CY8C95XXA_FUNC_SIZE	2
+static const char *cy8c95xxa_functions[CY8C95XXA_FUNC_SIZE] = {
+	"gpio",
+	"pwm",
+};
+
+static int cy8c95xxa_get_functions_count(struct pinctrl_dev *pctldev)
+{
+	return CY8C95XXA_FUNC_SIZE;
+}
+
+static const char *cy8c95xxa_get_function_name(struct pinctrl_dev *pctrldev,
+		unsigned selector)
+{
+	return cy8c95xxa_functions[selector];
+}
+
+static int cy8c95xxa_get_function_groups(struct pinctrl_dev *pctrldev,
+		unsigned selector, const char * const **groups, unsigned *num_groups)
+{
+	*groups = cy8c95xxa_group_names;
+	*num_groups = ARRAY_SIZE(cy8c95xxa_group_names);
+	return 0;
+}
+
+static int cy8c95xxa_set_mux(struct pinctrl_dev *pctrldev,
+		unsigned func_selector, unsigned group_selector)
+{
+	struct cy8c95xxa *cyp = pinctrl_dev_get_drvdata(pctrldev);
+	u8 port, bit;
+	int ret;
+
+	dev_info(cyp->dev, "%s called:", __func__);
+	dev_info(cyp->dev, "\tfunc_selector: %d", func_selector);
+	dev_info(cyp->dev, "\tgroup_selector: %d", group_selector);
+
+	if (func_selector != 1)
+		return -ENOTSUPP;
+
+	port = gpio_to_port(group_selector);
+	bit = gpio_to_bit(group_selector, port);
+
+	// select port
+	ret = cyp_write(cyp, REG_PORT_SELECT, port);
+	if (ret < 0) {
+		dev_err(cyp->dev, "Could not select port");
+		return ret;
+	}
+
+	// enable pwm
+	ret = cyp_write(cyp, REG_PWM_EAN, BIT(bit));
+	if (ret < 0) {
+		dev_err(cyp->dev, "Could not enable pwm");
+		return ret;
+	}
+
+	return 0;
+}
+
+static struct pinmux_ops cy8c95xxa_pinmux_ops = {
+	.get_functions_count = cy8c95xxa_get_functions_count,
+	.get_function_name = cy8c95xxa_get_function_name,
+	.get_function_groups = cy8c95xxa_get_function_groups,
+	.set_mux = cy8c95xxa_set_mux,
+};
+
+static int cy8c95xxa_pin_config_get(struct pinctrl_dev *pctldev,
+				    unsigned pin, unsigned long *config)
+{
+	return -ENOTSUPP;
+}
+
+static const u8 pin_config_to_reg[] = {
+	[PIN_CONFIG_BIAS_HIGH_IMPEDANCE] = REG_DRV_MODE_HIGH_Z,
+	[PIN_CONFIG_BIAS_PULL_DOWN] = REG_DRV_MODE_PULL_DWN,
+	[PIN_CONFIG_BIAS_PULL_UP] = REG_DRV_MODE_PULL_UP,
+	[PIN_CONFIG_DRIVE_OPEN_DRAIN] = REG_DRV_MODE_OPEN_DRAIN_LOW,
+	[PIN_CONFIG_DRIVE_OPEN_SOURCE] = REG_DRV_MODE_OPEN_DRAIN_HIGH,
+	[PIN_CONFIG_DRIVE_PUSH_PULL] = REG_DRV_MODE_STRONG,
+};
+
+static int cy8c95xxa_pin_config_set(struct pinctrl_dev *pctldev,
+				    unsigned pin,
+				    unsigned long *configs,
+				    unsigned num_configs)
+{
+	struct cy8c95xxa *cyp = pinctrl_dev_get_drvdata(pctldev);
+	enum pin_config_param param;
+	u8 port, bit, value;
+	int ret;
+
+	port = gpio_to_port(pin);
+	bit = gpio_to_bit(pin, port);
+
+	dev_info(cyp->dev, "For pin %d the parameters are:", pin);
+
+	/* only bother to apply the LAST config, since that's the
+	 * one that sicks anyway. */
+	param = pinconf_to_config_param(configs[num_configs - 1]);
+	dev_info(cyp->dev, "\tlast parameter: %d, register: 0x%x",
+		 param, pin_config_to_reg[param]);
+
+	value = BIT(bit);
+
+	ret = cyp_write(cyp, REG_PORT_SELECT, port);
+	if (ret < 0)
+		return ret;
+
+	ret = cyp_write(cyp, pin_config_to_reg[param], value);
+	if (ret < 0)
+		return ret;
+
+	ret = cyp_read(cyp, pin_config_to_reg[param]);
+	if (ret < 0)
+		return ret;
+
+	dev_info(cyp->dev, "READ: 0x%x", ret);
+
+	return 0;
+}
+
+static const struct pinconf_ops cy8_pinconf_ops = {
+	.pin_config_get = cy8c95xxa_pin_config_get,
+	.pin_config_set = cy8c95xxa_pin_config_set,
+	.is_generic = true,
+};
+
+static int cy8c95xxa_get_groups_count(struct pinctrl_dev *pctrldev)
+{
+	return ARRAY_SIZE(cy8c95xxa_group_names);
+}
+
+static const char *cy8c95xxa_get_group_name(struct pinctrl_dev *pctldev,
+					    unsigned selector)
+{
+	return cy8c95xxa_group_names[selector];
+}
+
+static const struct pinctrl_ops cy8_pinctrl_ops = {
+	.get_groups_count = cy8c95xxa_get_groups_count,
+	.get_group_name = cy8c95xxa_get_group_name,
+	.dt_node_to_map = pinconf_generic_dt_node_to_map_all,
+	.dt_free_map = pinconf_generic_dt_free_map,
+};
+
+static int cy8c95xxa_pwm_apply(struct pwm_chip *chip, struct pwm_device *pwm,
+		const struct pwm_state *state)
+{
+	struct cy8c95xxa *cyp = container_of(chip, struct cy8c95xxa, pwm_chip);
+	u64 period = state->period;
+	u64 duty = state->duty_cycle;
+	int ret;
+
+	// TODO: apply enable...
+
+	// TODO: for now, lock the clock frequency to the default 32kHz.
+	period = __div64_const32(period, PWM_CLK_NS);
+	duty = __div64_const32(duty, PWM_CLK_NS);
+
+	// TODO: check the bounds of period!
+
+	dev_info(cyp->dev, "%s called!", __func__);
+	dev_info(cyp->dev, "\t For pwm: %d", pwm->pwm);
+	dev_info(cyp->dev, "\t Period: %lld", period);
+	dev_info(cyp->dev, "\t Duty: %lld", duty);
+
+	ret = cyp_write(cyp, REG_PWM_SELECT, pwm->pwm);
+	if (ret < 0) {
+		dev_err(cyp->dev, "Could not select pwm %d", pwm->pwm);
+		return ret;
+	}
+
+	ret = cyp_write(cyp, REG_PWM_PERIOD, period);
+	if (ret < 0) {
+		dev_err(cyp->dev, "Could not set period");
+		return ret;
+	}
+
+	ret = cyp_write(cyp, REG_PWM_PULSE_W, duty);
+	if (ret < 0) {
+		dev_err(cyp->dev, "Could not set pulse width");
+		return ret;
+	}
+
+	return 0;
+}
+
+static void cy8c95xxa_pwm_get_state(struct pwm_chip *chip,
+		struct pwm_device *pwm, struct pwm_state *state)
+{
+	struct cy8c95xxa *cyp = container_of(chip, struct cy8c95xxa, pwm_chip);
+
+	dev_info(cyp->dev, "%s called!", __func__);
+	dev_info(cyp->dev, "\t For pwm: %d", pwm->pwm);
+}
+
+static const struct pwm_ops cy8c95xxa_pwm_ops = {
+	.apply = cy8c95xxa_pwm_apply,
+	.get_state = cy8c95xxa_pwm_get_state,
+};
 
 /**
  * cy8c9520a_irq_mask - mask (disable) an irq
@@ -581,9 +774,21 @@ static int cy8c95xxa_probe(struct i2c_client *client)
 
 	init_gpio_chip(cyp);
 
+	cyp->pwm_chip.dev = cyp->dev;
+	cyp->pwm_chip.ops = &cy8c95xxa_pwm_ops;
+	cyp->pwm_chip.base = PWM_BASE;
+	cyp->pwm_chip.npwm = NPWM;
+
+	ret = devm_pwmchip_add(cyp->dev, &cyp->pwm_chip);
+	if (ret < 0) {
+		dev_err(cyp->dev, "Could not add pwm chip");
+		return ret;
+	}
+
 	cyp->pinctrl.name = "cyp8c95xxa-pinctrl";
 	cyp->pinctrl.pctlops = &cy8_pinctrl_ops;
 	cyp->pinctrl.confops = &cy8_pinconf_ops;
+	cyp->pinctrl.pmxops = &cy8c95xxa_pinmux_ops;
 	cyp->pinctrl.pins = pinctrl_pins_20a;
 	cyp->pinctrl.npins = ARRAY_SIZE(pinctrl_pins_20a);
 	cyp->pinctrl.owner = THIS_MODULE;
